2d02ed48c5266523ff0fee8c4df73239c6859fdc
ratnesh0096 <ratneshmether09.rm@gmail.com>
2021-05-11 21:31:31 +0530

    Hello

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = { flag: false }
+		// console.log(this.flag);
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				{this.state.flag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+				<button id="click" onClick={() => this.setState({ flag: true })}>Hey! Click Me to see Magic.</button>
+			</div>
+		);
+	}
 }
 
 
